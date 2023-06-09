24b41db4a8bb5208041c1a3015469a9965fbd7dc
Krecichwost <kamilkre@amazon.com>
2023-06-09 13:25:02 +0200

    I'm not sure if that works

--- Ml/src/ml_endpoint.py
+++ Ml/src/ml_endpoint.py
@@ -1,8 +1,11 @@
-from typing import Optional, List
-from fastapi import FastAPI, HTTPException
+import schedule
+import time
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pydantic import BaseModel
+from typing import Optional, List
 from fastapi.middleware.cors import CORSMiddleware
 from price_prediction import predict_price
+import requests
 import json
 
 class MLData(BaseModel):
@@ -34,13 +37,30 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
+def job():
+    response = requests.get('http://localhost:8069/get_offers')  # replace with your endpoint
+    data = response.json()
 
-@app.post(" ")
-async def save_learning_data(ml_data: MLData):
-    # Save JSON data to a file
-    with open(ml_data.file_path, 'w') as f:
-        json.dump(ml_data.data, f)
-    return {"message": "Data saved successfully"}
+    data_important = json()
+    for apartment in data:
+        data_important[apartment["id"]]["price"] = apartment["price"]
+        data_important[apartment["id"]]["area"] = apartment["area"]
+        data_important[apartment["id"]]["rooms_amount"] = apartment["rooms_amount"]
+    with open('ml_data.json', 'w') as f:
+        json.dump(data_important.data, f)
+
+schedule.every(6).hours.do(job)
+
+def run_scheduler():
+    job()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+@app.on_event("startup")
+async def startup_event():
+    task = BackgroundTasks()
+    task.add_task(run_scheduler)
 
 @app.get("/price_prediction")
 async def get_price_prediction(predict_data: PredictData):
